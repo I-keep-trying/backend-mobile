59ab4e9fad4a8692b2e55be9d3f140246a8ec446
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-05-09 12:29:36 -0400

    map features added

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const compression = require('compression')
 const helmet = require('helmet')
 const path = require('path')
 const app = express()
+const fs = require('fs')
 const moment = require('moment')
+
+const date = new Date()
+const today = moment(date).format('YYYY-MM-DD')
 
 app.use((req, res, next) => {
   res.setHeader('Permissions-Policy', 'geolocation=(), interest-cohort=()')
@@ -25,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use(compression())
 app.use(express.json())
-app.use(middleware.requestLogger)
+//app.use(middleware.requestLogger)
 
 app.use(express.static('build'))
 
@@ -87,24 +91,80 @@ app.get('/api/time/lat/:lat/lng/:lng', async (req, res) => {
 })
 
 // ---------------------- News ----------------------------
-const date = new Date()
-const today = moment(date).format('YYYY-MM-DD')
 
 app.get('/api/countries/news/:name', async (req, res) => {
   const selectedCountry = req.params.name
   try {
+    // const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${selectedCountry}&apiKey=${process.env.REACT_APP_NEWSAPI_KEY}`)
+    //    console.log('response.data', response.data)
     const response = await axios.get(
       `https://newsapi.org/v2/everything?q=${selectedCountry}&from=${today}&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWSAPI_KEY}`
     )
     res.json(response.data)
   } catch (err) {
     console.log('axios request failed api/countries/news', err)
-    // res.status(err.response.status).send(err.response.statusText)
+    res.status(err.response.status).send(err.response.statusText)
   }
 })
 
+// --------------------- Maps --------------------------------
+//// --------------------- Maps.bbox --------------------------------
+
+app.get('/api/countries/map/:name', async (req, res) => {
+  const selectedCountry = req.params.name
+  try {
+    const response = await axios.get(
+      `https://nominatim.openstreetmap.org/search?q=${selectedCountry}&polygon_geojson=1&format=json`
+    )
+    const resData = response.data[0].boundingbox
+    const arrResData = [
+      Number(resData[0]),
+      Number(resData[1]),
+      Number(resData[2]),
+      Number(resData[3]),
+    ]
+    res.json(arrResData)
+  } catch (err) {
+    console.log('axios request failed api/countries/map', err)
+    res.status(err.response.status).send(err.response.statusText)
+  }
+})
+
+//// --------------------- Maps.features --------------------------------
+
+app.get('/api/countries/features/lat/:lat/lon/:lon', async (req, res) => {
+  const location = req.params
+  try {
+    const response = await axios.get(
+      `https://api.geoapify.com/v2/places?categories=accommodation.hotel&filter=circle:${location.lon},${location.lat},5000&limit=20&apiKey=${process.env.REACT_APP_GEOAPIFY_KEY}`
+    )
+    res.json(response.data)
+  } catch (err) {
+    console.log('axios request failed api/countries/features', err)
+    res.status(err.response.status).send(err.response.statusText)
+  }
+})
+
+//// --------------------- Maps.features.details --------------------------------
+
+app.get('/api/countries/feature/:id', async (req, res) => {
+  console.log('feature req', req.params.id)
+  const id = req.params.id
+  try {
+    const response = await axios.get(
+      `https://api.geoapify.com/v2/place-details?id=${id}&apiKey=${process.env.REACT_APP_GEOAPIFY_KEY}`
+    )
+    res.json(response.data)
+  } catch (err) {
+    console.log('axios request failed api/countries/feature', err)
+    res.status(err.response.status).send(err.response.statusText)
+  }
+})
+
+// ------------------ global functions -----------------------
+
 const unknownEndpoint = (request, response) => {
-  console.log('Unknown endpoint error')
+  console.log('Unknown endpoint error', request.params)
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
